382ab56a0dd401fadf94cee86f8b47ad82ae6667
samarthshubham <shubhamsamarth@outlook.com>
2022-01-21 03:44:04 +0530

    manage user with profile context; put real time subs

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const config = {
-  apiKey: 'AIzaSyCGWb4hrkrUPkK-euc6yVl2pG0R0VQtrMM',
-  authDomain: 'chat-web-app-315f4.firebaseapp.com',
-  databaseURL: 'https://chat-web-app-315f4-default-rtdb.firebaseio.com',
-  projectId: 'chat-web-app-315f4',
-  storageBucket: 'chat-web-app-315f4.appspot.com',
-  messagingSenderId: '923342820195',
-  appId: '1:923342820195:web:19062eb18f02de592cb956',
+  apiKey: "AIzaSyCGWb4hrkrUPkK-euc6yVl2pG0R0VQtrMM",
+  authDomain: "chat-web-app-315f4.firebaseapp.com",
+  databaseURL: "https://chat-web-app-315f4-default-rtdb.firebaseio.com",
+  projectId: "chat-web-app-315f4",
+  storageBucket: "chat-web-app-315f4.appspot.com",
+  messagingSenderId: "923342820195",
+  appId: "1:923342820195:web:19062eb18f02de592cb956"
 };
 
 const app = firebase.initializeApp(config);
