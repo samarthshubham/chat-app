0700637dd0447fe820db725e643170aa2d095c74
samarthshubham <shubhamsamarth@outlook.com>
2022-01-21 01:56:43 +0530

    intialised firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: "AIzaSyCGWb4hrkrUPkK-euc6yVl2pG0R0VQtrMM",
+  authDomain: "chat-web-app-315f4.firebaseapp.com",
+  projectId: "chat-web-app-315f4",
+  storageBucket: "chat-web-app-315f4.appspot.com",
+  messagingSenderId: "923342820195",
+  appId: "1:923342820195:web:19062eb18f02de592cb956"
+};
+
+
+const app = firebase.initializeApp(config);
+
