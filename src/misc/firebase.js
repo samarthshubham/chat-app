c0af55261d673663fbeaa47f16cac924cfd60ac2
samarthshubham <shubhamsamarth@outlook.com>
2022-01-21 02:37:37 +0530

    created public and private routes

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase/app';
 
 const config = {
-  apiKey: "AIzaSyCGWb4hrkrUPkK-euc6yVl2pG0R0VQtrMM",
-  authDomain: "chat-web-app-315f4.firebaseapp.com",
-  projectId: "chat-web-app-315f4",
-  storageBucket: "chat-web-app-315f4.appspot.com",
-  messagingSenderId: "923342820195",
-  appId: "1:923342820195:web:19062eb18f02de592cb956"
+  apiKey: 'AIzaSyAKGRssxY4OW9um6RlSbbQ1Fh_lgdWfoPs',
+  authDomain: 'chat-web-app-4ee4c.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-4ee4c.firebaseio.com',
+  projectId: 'chat-web-app-4ee4c',
+  storageBucket: 'chat-web-app-4ee4c.appspot.com',
+  messagingSenderId: '167319830934',
+  appId: '1:167319830934:web:419e220f9fdcd15ea25db7',
 };
 
-
 const app = firebase.initializeApp(config);
-
