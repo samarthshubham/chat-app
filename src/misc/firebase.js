6ac49b5bae0fa80c5cb92d3d2c83d74129ccd2cc
samarthshubham <shubhamsamarth@outlook.com>
2022-01-23 01:30:03 +0530

    added firebase messaging and store user token in the db

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
+import 'firebase/messaging';
 
 const config = {
   apiKey: "AIzaSyCGWb4hrkrUPkK-euc6yVl2pG0R0VQtrMM",
@@ -17,3 +18,17 @@ const app = firebase.initializeApp(config);
 export const auth = app.auth();
 export const database = app.database();
 export const storage = app.storage();
+
+export const messaging = firebase.messaging.isSupported()
+  ? app.messaging()
+  : null;
+
+if (messaging) {
+  messaging.usePublicVapidKey(
+    'BMaaGI-DYabir8IV99i7cUp9CYGXiaPe_T_RvrnbsN7PW7uljR-vN-9fNcDPtyKx5YB4iY695G03Lbt7VZsdBBY'
+  );
+
+  messaging.onMessage(data => {
+    console.log(data);
+  });
+}
