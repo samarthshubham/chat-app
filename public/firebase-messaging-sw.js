bbf5629f88cc8afc43fa14beedd758239b42538f
samarthshubham <shubhamsamarth@outlook.com>
2022-01-23 02:02:01 +0530

    added service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,13 +5,13 @@ importScripts(
 );
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyAKGRssxY4OW9um6RlSbbQ1Fh_lgdWfoPs',
-  authDomain: 'chat-web-app-4ee4c.firebaseapp.com',
-  databaseURL: 'https://chat-web-app-4ee4c.firebaseio.com',
-  projectId: 'chat-web-app-4ee4c',
-  storageBucket: 'chat-web-app-4ee4c.appspot.com',
-  messagingSenderId: '167319830934',
-  appId: '1:167319830934:web:419e220f9fdcd15ea25db7',
+  apiKey: "AIzaSyCGWb4hrkrUPkK-euc6yVl2pG0R0VQtrMM",
+  authDomain: "chat-web-app-315f4.firebaseapp.com",
+  databaseURL: "https://chat-web-app-315f4-default-rtdb.firebaseio.com",
+  projectId: "chat-web-app-315f4",
+  storageBucket: "chat-web-app-315f4.appspot.com",
+  messagingSenderId: "923342820195",
+  appId: "1:923342820195:web:19062eb18f02de592cb956"
 });
 
 firebase.messaging();
